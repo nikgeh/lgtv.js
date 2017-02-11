bbf6086fc55bcc3575fdd09793affff1d9fabdb4
nikgeh <nikgeh@gmail.com>
2017-02-10 23:44:53 -0800

    Support turning off tv

---
+++ examples/turn-off.js
@@ -0,0 +1,20 @@
+lgtv = require("lgtv");
+/*---------------------------------------------------------------------------*/
+var retry_timeout = 10;
+var run_test = function() {
+    lgtv.discover_ip(retry_timeout, function(err, ipaddr) {
+      if (err) {
+        console.log("Failed to find TV IP address on the LAN. Verify that TV is on, and that you are on the same LAN/Wifi.");
+    
+      } else {
+        console.log("Found TV at address " + ipaddr + ", running example.");
+        lgtv.connect(ipaddr, function(err, response){
+          if (!err) {
+            lgtv.turn_off();
+          }
+        });
+      }
+    });
+};
+console.log("turning off TV");
+run_test();
