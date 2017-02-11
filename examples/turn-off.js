85ee66f57c7285de7aee97a55202208a679a9211
nikgeh <nikgeh@gmail.com>
2017-02-11 11:52:03 -0800

    Turn off script

--- examples/turn-off.js
+++ examples/turn-off.js
@@ -2,19 +2,12 @@ lgtv = require("lgtv");
 /*---------------------------------------------------------------------------*/
 var retry_timeout = 10;
 var run_test = function() {
-    lgtv.discover_ip(retry_timeout, function(err, ipaddr) {
-      if (err) {
-        console.log("Failed to find TV IP address on the LAN. Verify that TV is on, and that you are on the same LAN/Wifi.");
-    
-      } else {
-        console.log("Found TV at address " + ipaddr + ", running example.");
+  var ipaddr = "192.168.2.2";
         lgtv.connect(ipaddr, function(err, response){
           if (!err) {
             lgtv.turn_off();
           }
         });
-      }
-    });
 };
 console.log("turning off TV");
 run_test();
