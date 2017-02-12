be8f46eef30865b4c808bee80aa513670e243ae0
nikgeh <nikgeh@gmail.com>
2017-02-11 20:56:10 -0800

    Disconnect when turning off tv

--- examples/turn-off.js
+++ examples/turn-off.js
@@ -3,9 +3,10 @@ lgtv = require("lgtv");
 var retry_timeout = 10;
 var run_test = function() {
   var ipaddr = "192.168.2.2";
-        lgtv.connect(ipaddr, function(err, response){
+        lgtv.connect(ipaddr, function(err, response) {
           if (!err) {
             lgtv.turn_off();
+            lgtv.disconnect();
           }
         });
 };
